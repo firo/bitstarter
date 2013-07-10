a303d9ea5d093af8d58108413819a087a92c82a8
Ferruccio Irionè <me@firo.it>
2013-07-10 15:04:21 +0200

    Add reading text from file

--- web.js
+++ web.js
@@ -1,9 +1,26 @@
+var fs = require('fs');
 var express = require('express');
+var fileName = './index.html';
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	
+	fs.exists(fileName, function(exists) {
+		if (exists) {
+			fs.stat(fileName, function(error, stats) {
+				fs.open(fileName, 'r', function(error, fd) {
+					var buffer = new Buffer(stats.size);
+					fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+						var data = buffer.toString('utf8', 0, buffer.length);
+						response.send(data.toString('utf8'));
+						fs.close(fd);
+					});
+				});
+			});
+		}
+	});
 });
 
 var port = process.env.PORT || 5000;
